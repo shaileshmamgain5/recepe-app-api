cd7d5888e6d7de305858c6a57013de153683f87e
Shailesh Mamgain <shaileshmamgain5@gmail.com>
2024-08-14 19:51:45 -0400

    linting

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,13 +6,12 @@ from django.test import SimpleTestCase
 from app import calc
 
 
-class CalcTests (SimpleTestCase):
+class CalcTests(SimpleTestCase):
     """Test the calc module."""
 
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual (res, 11)
-
 
     def test_subtract_number(self):
         res = calc.subtract(10, 15)
