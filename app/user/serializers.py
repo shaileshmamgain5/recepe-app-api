f7434ca615109ad43a0a4c5701c5ba86dc662f6f
Shailesh Mamgain <shaileshmamgain5@gmail.com>
2024-09-01 21:39:19 -0400

    update api added

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -23,6 +23,18 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted password. """
         return get_user_model().objects.create_user(**validated_data)
 
+    # model instance that is being updated
+    def update(self, instance, validated_data):
+        """Update and return a user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token"""
