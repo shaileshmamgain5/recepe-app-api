214e6e20b7ee685cae9b665455727cb043aae67b
Shailesh Mamgain <shaileshmamgain5@gmail.com>
2024-09-03 17:24:55 -0400

    assigned_only filter added with doc schema

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -1,6 +1,8 @@
 """
 Tests for the Tags API.
 """
+from decimal import Decimal
+
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
@@ -8,7 +10,10 @@ from django.test import TestCase
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from core.models import Tag
+from core.models import (
+    Tag,
+    Recipe
+)
 
 from recipe.serializers import TagSerializer
 
@@ -93,3 +98,45 @@ class PrivateTagsAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
         tags = Tag.objects.filter(user=self.user)
         self.assertFalse(tags.exists())
+
+    def test_filter_tags_assigned_to_recipe(self):
+        """Test listing tags by those assigned to recipes"""
+        tag1 = Tag.objects.create(user=self.user, name='Tag1')
+        tag2 = Tag.objects.create(user=self.user, name='Tag2')
+        recipe = Recipe.objects.create(
+            title='AppleTitle',
+            time_minutes=5,
+            price=Decimal('4.5'),
+            user=self.user
+        )
+        recipe.tags.add(tag1)
+
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+
+        s1 = TagSerializer(tag1)
+        s2 = TagSerializer(tag2)
+        self.assertIn(s1.data, res.data)
+        self.assertNotIn(s2.data, res.data)
+
+    def test_filtered_tags_unique(self):
+        """Test filtered tags returns a unique list."""
+        tag = Tag.objects.create(user=self.user, name='Eggs')
+        Tag.objects.create(user=self.user, name='TagOther')
+        recipe1 = Recipe.objects.create(
+            title='AppleTitle',
+            time_minutes=5,
+            price=Decimal('4.5'),
+            user=self.user
+        )
+        recipe2 = Recipe.objects.create(
+            title='Eggs Bendict Title',
+            time_minutes=5,
+            price=Decimal('4.5'),
+            user=self.user
+        )
+        recipe1.tags.add(tag)
+        recipe2.tags.add(tag)
+
+        res = self.client.get(TAGS_URL, {'assigned_only': 1})
+
+        self.assertEqual(len(res.data), 1)
